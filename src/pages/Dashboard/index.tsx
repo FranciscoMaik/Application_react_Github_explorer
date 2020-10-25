5a4c1283b48c7653baff18dccce691253105bc96
FranciscoMaik <chiconunes3081@gmail.com>
2020-10-25 17:54:28 -0300

    configuration of eslint and layout of page Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
 
-import { Title } from './style';
+import { Title, Form, Repositories } from './style';
 
-const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github.</Title>;
-}
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Github Explorer" />
+    <Title>Explore repositórios no Github.</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="teste">
+        <img src="https://avatars1.githubusercontent.com/u/20601076?s=460&u=164dd0918a0eb4ffcd0615290fc198453c9461be&v=4" alt="Francisco Maik" />
+        <div>
+          <strong>trabalhando</strong>
+          <p>descrição</p>
+        </div>
+
+        <FiChevronRight size={20} />
+
+      </a>
+
+      <a href="teste">
+        <img src="https://avatars1.githubusercontent.com/u/20601076?s=460&u=164dd0918a0eb4ffcd0615290fc198453c9461be&v=4" alt="Francisco Maik" />
+        <div>
+          <strong>trabalhando</strong>
+          <p>descrição</p>
+        </div>
+
+        <FiChevronRight size={20} />
+
+      </a>
+
+      <a href="teste">
+        <img src="https://avatars1.githubusercontent.com/u/20601076?s=460&u=164dd0918a0eb4ffcd0615290fc198453c9461be&v=4" alt="Francisco Maik" />
+        <div>
+          <strong>trabalhando</strong>
+          <p>descrição</p>
+        </div>
+
+        <FiChevronRight size={20} />
+
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 // import { Container } from './styles';
 
-const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
-}
+const Repository: React.FC = () => <h1>Repository</h1>;
 
 export default Repository;
