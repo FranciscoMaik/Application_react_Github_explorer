ee77e706e06d7e41cb0f017f86af3197120aa19d
FranciscoMaik <chiconunes3081@gmail.com>
2020-10-26 14:09:20 -0300

    Stylization from Repository page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-// import { Container } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams{
     repository: string;
@@ -10,7 +13,51 @@ interface RepositoryParams{
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Description</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <p>Stars</p>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <p>Stars</p>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <p>Stars</p>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="alfdhgsd">
+          <div>
+            <strong>afgsdfg</strong>
+            <p>asdfasdf</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
